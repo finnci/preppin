6417f180b6dfa0168d1ea5024a2815ac9fdd52e9
finnci <4cfinn@gmail.com>
2019-10-03 16:08:16 +0100

    newloops

--- loopy.py
+++ loopy.py
@@ -1,59 +1,133 @@
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
 
 
-def get_elems(input_data, given_index, llen):
-    given_val = input_data[given_index]
-    range_val = max([given_index, (llen - given_index)])
+'''
+Some notes this answer:
+After a few iterations of splitting lists & trying to sort input i
+settled on a pretty clean approach (in my opinion anyway):
+When given some index Y for the list X, iterate in incremental
+distances either side of X[Y] (starting with left) and returning
+the first index which has a value lower than the one found at X[Y]
+
+I added a simple cache to the query loop to check if we have already
+figured out an answer for Y
+
+Some pre-optimising is done by checking if
+the X[Y] is lower than the minimum value in X.
+
+I tested out a few options for sorting queries to improve cache
+performance, and while there might have been improvements, it wasn't
+enough to pass "case 10" so I left it out as i felt the sorting of the 
+queries and then sorting the results was less clean to read.
+
+Which brings me to "test case 10"...
+So, it won't execute in time, i guess there's some really expensive/worst case
+input. To get the speed required for this, I probably need to redisign some parts of 
+my answer, i considered a few options:
+- balanced binary tree sort of structure
+- maybe something clever with hashtables
+- multiprocessing (reduced cache performance though)
+
+Ultimately, I didn't have enough time to get this properly redisigned and make it pass,
+i'd love to see the input/queries for that test case if someone would be able to send them on
+to me, even just to play around with on my own.
+'''
+
+def get_elems(input_data, given_index, llen, given_val):
+    '''
+    Takes a list of ints, an index in that list and the len of the input.
+    Returns the first index left or right of the given_index with a
+    value lower than the value at the given_index
+    '''
+    range_val = max([given_index, (llen - given_index)]) 
+    # iterate over numbers from 1 -> (given_index or len - given_index)
+    # explained:
+    #    - we will index to i+given_index in the following loop.
+    #    - range_val + given_index is the same as the len
     for i in range(1, range_val):
-        if given_index - i >= 0:
-            left_elem = input_data[given_index - i]
-            if left_elem < given_val:
-                return given_index - i + 1
-        if given_index + i < llen:
-            right_elem = input_data[given_index + i]
-            if right_elem < given_val:
-                return given_index + i + 1
+        # check to the left of the
+        # given index.
+        left_index = given_index - i
+        if left_index >= 0 and input_data[left_index] < given_val:
+            # this is the first element lower than
+            # the given val, it is our answer. (+1 )
+            return left_index + 1
+        # left didn't pass, lets check right.
+        right_index = given_index + i
+        if right_index < llen and input_data[right_index] < given_val:
+            # this is the first element lower than
+            # the given val, it is our answer because
+            # we have already checked the left side.
+            return right_index + 1
+    # no answer, return -1
     return -1
 
 
 def predictAnswer(stockData, queries):
+    '''
+    Given 2 lists, 
+    For queries[i] find closest index with a 
+    value lower than the value at stockData[i]
+    Returning a list of those closest indexes
+    '''
     res = []
-    cache = {}
-    #min_in = min(stockData)
+    cache= {}
+    # pre-process a few vals to save some time
+    # later on.
+    min_in = min(stockData)
     llen = len(stockData)
     for inp in queries:
+        # offset of -1 for list indexing
         inp -= 1
-        # try
-        #     res.append(cache[inp])
-        #     continue
-        # except:
-        #     # just pass
-        #     pass
         if cache.get(inp):
+            # check our cache to see
+            # if we already got this answer
             res.append(cache[inp])
             continue
-        #if stockData[inp] == min_in:
-        #    res.append(-1)
-        #    cache[inp] = -1
-        #    continue
-        r = get_elems(stockData, inp, llen)
+        # not in cache, need the value rn
+        this_val = stockData[inp]
+        if this_val <= min_in:
+            # edge case catch:
+            # if the val@index <= min, answer is -1
+            res.append(-1)
+            cache[inp] = -1
+            continue
+        r = get_elems(stockData, inp, llen, this_val)
+        # we have an answer - update cache and append result.
         cache[inp] = r
         res.append(r)
     return res
 
 
-if __name__ == "__main__":
-    #    0, 1, 2, 3
-    #    1, 2, 3, 4, 5, 6  7,  8, 9, 10
-    #input_data = [5, 6, 8, 4, 9, 10, 8, 3, 6, 4]
-    input_data = [89214, 26671, 75144, 32445, 13656, 66289, 21951, 10265, 59857, 59133, 63227, 86121, 37411, 54628, 25859, 43510, 63756, 54763, 30852, 53243, 76238, 96885, 33074, 17745, 81814, 43436, 79172, 92819, 30001, 68442, 54021, 35566, 95113, 29164, 84362, 25120, 11804, 6313, 51736, 71661, 81797, 14962, 57781, 35560, 85941, 99991, 95421, 66048, 54754, 26272, 35642, 47343, 39508, 85068, 65087, 21321, 28503, 60611, 30491, 58503, 29052, 84512, 94069, 40516, 13675, 78430, 65635, 25479, 1094, 17370, 13491, 99243, 48683, 71271, 34802, 34624, 87613, 46574, 671, 42366, 89197, 36313, 89708, 28704, 21380, 54795, 66376, 49882, 15405, 96867, 24737, 60808, 81378, 35157, 1324, 11404, 29938, 66958, 53234, 47384]
-    
-    queries = [80, 24, 26, 62, 46, 79, 85, 59, 52, 8, 76, 48, 72, 84, 3, 3, 30, 30, 36, 86, 96, 72, 93, 25, 28, 68, 81, 18, 78, 14, 1, 57, 90, 26, 18, 87, 56, 55, 97, 59, 62, 73, 58, 85, 8, 60, 87, 89, 89, 22]
-    expected = [79 , 37 , 24 , 61 , 45 , -1 , 89 , 57 , 51 , 38 , 79 , 49 , 71 , 85 , 2 , 2 , 29 , 29 , 37 , 85 , 95 , 71 , 92 , 24 , 27 , 69 , 80 , 19 , 79 , 13 , 2 , 56 , 89 , 24 , 19 , 86 , 65 , 56 , 96 , 57 , 61 , 71 , 57 , 89 , 38 , 59 , 86 , 95 , 95 , 21]
-    #expected = [2,4,-1]
-    #queries = [3, 1, 8]
-    res = predictAnswer(input_data, queries)
-    print(expected)
-    print(res)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
+    stockData_count = int(input().strip())
 
+    stockData = []
+
+    for _ in range(stockData_count):
+        stockData_item = int(input().strip())
+        stockData.append(stockData_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = int(input().strip())
+        queries.append(queries_item)
+
+    result = predictAnswer(stockData, queries)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
